e8a7370a0a7a772a729a3abfaf5943b474333b0b
Mohit Sharma <sharma_mohit@sharma.mohit>
2018-06-17 02:29:25 +0530

    Added delete todos function

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('5b25766526e06f40853952f1').then((todo) => {
+  console.log(todo);
+});

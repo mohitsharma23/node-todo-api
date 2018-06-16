d06aeda6e96f698039e22b789ac1296b4e9c41b8
Mohit Sharma <sharma_mohit@sharma.mohit>
2018-06-17 03:43:25 +0530

    added update todos

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {$new: true}).then((todo) => {
+  Todo.findByIdAndUpdate(id, {$set: body}, {$new: false}).then((todo) => {
     if(!todo){
       return res.status(404).send();
     }
